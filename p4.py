92481c68879b67e6d8cfed46eb82fb2cec36da7d
Witalis Domitrz <witekdomitrz@gmail.com>
2023-12-26 23:01:42 +0000

    4

---
+++ p4.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+with open(Path("i4.txt"), "r", encoding="utf8") as f:
+    data = f.read().strip().splitlines()
+
+
+def count(line):
+    _, line = line.split(": ")
+    s1, s2 = line.split(" | ")
+    s1, s2 = set(s1.split()), set(s2.split())
+    s = s1 & s2
+    return len(s)
+
+
+def do1(line):
+    r = count(line)
+    if r == 0:
+        return 0
+    return 2 ** (r - 1)
+
+
+print(sum(do1(line) for line in data))
+
+
+def do2(data):
+    counts = [1 for _ in data]
+    for i, line in enumerate(data):
+        r = count(line)
+        for j in range(i + 1, i + r + 1):
+            counts[j] += counts[i]
+    return sum(counts)
+
+
+print(do2(data))
