8c21cc44308fa5a786b47c2e61e38b429e15226c
Witalis Domitrz <witekdomitrz@gmail.com>
2023-12-27 01:45:02 +0000

    9

---
+++ p9.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+with open(Path("i9.txt"), "r", encoding="utf8") as f:
+    data = f.read().strip().splitlines()
+
+
+def helper(curr_data):
+    if curr_data == [0] * len(curr_data):
+        return 0
+    diffs = [y - x for x, y in zip(curr_data, curr_data[1:])]
+    return curr_data[-1] + helper(diffs)
+
+
+def do1(line):
+    line = list(map(int, line.split()))
+    return helper(line)
+
+
+print(sum(do1(line) for line in data))
+
+
+def helper2(curr_data):
+    if curr_data == [0] * len(curr_data):
+        return 0
+    diffs = [y - x for x, y in zip(curr_data, curr_data[1:])]
+    return curr_data[0] - helper2(diffs)
+
+
+def do2(line):
+    line = list(map(int, line.split()))
+    return helper2(line)
+
+
+print(sum(do2(line) for line in data))
